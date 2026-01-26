8000a873c16ccaf85d00c3e78a1c212e6f52b945
alex <a.gilmore1993@gmail.com>
2026-01-26 08:35:55 -0600

    update default values in core schema

--- data_model/core.py
+++ data_model/core.py
@@ -8,26 +8,25 @@ from typing import Optional
 class Database(BaseModel):
     id: str = Field(..., description="The unique identifier for the database")
     name: str = Field(..., description="The name of the database")
-    description: str = Field(..., description="The description of the database")
-    embedding: Optional[list[float]] = Field(None, description="The embedding of the database description")
+    description: Optional[str] = Field(default=None, description="The description of the database")
+    embedding: Optional[list[float]] = Field(default=None, description="The embedding of the database description")
 
 class Table(BaseModel):
     id: str = Field(..., description="The unique identifier for the table")
     name: str = Field(..., description="The name of the table")
-    description: str = Field(..., description="The description of the table")
-    embedding: Optional[list[float]] = Field(None, description="The embedding of the table description")
+    description: Optional[str] = Field(default=None, description="The description of the table")
+    embedding: Optional[list[float]] = Field(default=None, description="The embedding of the table description")
 
 class Column(BaseModel):
     id: str = Field(..., description="The unique identifier for the column")
     name: str = Field(..., description="The name of the column")
-    description: str = Field(..., description="The description of the column")
-    embedding: Optional[list[float]] = Field(None, description="The embedding of the column description")
+    description: Optional[str] = Field(default=None, description="The description of the column")
+    embedding: Optional[list[float]] = Field(default=None, description="The embedding of the column description")
     type: str = Field(..., description="The data type of the column")
-    nullable: bool = Field(..., description="Whether the column can be null")
-    is_primary_key: bool = Field(..., description="Whether the column is a primary key")
-    is_foreign_key: bool = Field(..., description="Whether the column is a foreign key")
+    nullable: bool = Field(default=True, description="Whether the column can be null")
+    is_primary_key: bool = Field(default=False, description="Whether the column is a primary key")
+    is_foreign_key: bool = Field(default=False, description="Whether the column is a foreign key")
 
-    # TODO: need to validate both pk and fk are both not True
     @field_validator('is_foreign_key', mode='after')
     def validate_not_both_pk_and_fk(cls, v: bool, info: ValidationInfo) -> bool:
         """
