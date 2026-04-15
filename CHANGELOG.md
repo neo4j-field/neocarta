ee989b2eb6dba1d286f4f345fcdfe53cb0c88c1e
alex <73862627+a-s-g93@users.noreply.github.com>
2026-04-15 14:01:03 -0500

    Prep v0.1.0 release (#90)
    * update publish action, add changelog, add smoke tests
    
    * update README and makefile commands
    
    * add version to library __init__, fix python version constraint in README
    
    * Update test_imports.py

---
+++ CHANGELOG.md
@@ -0,0 +1,13 @@
+# Changelog of neocarta library
+
+## Upcoming
+
+### Fixed
+
+### Changed
+
+### Added
+
+## v0.1.0
+
+Initial release
--- neocarta/__init__.py
+++ neocarta/__init__.py
@@ -1,5 +1,9 @@
 """Neocarta library root package."""
 
+from importlib.metadata import version
+
 from .enums import NodeLabel, RelationshipType
 
-__all__ = ["NodeLabel", "RelationshipType"]
+__version__ = version("neocarta")
+
+__all__ = ["NodeLabel", "RelationshipType", "__version__"]
